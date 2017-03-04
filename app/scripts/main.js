824b00c4ed1f080a847dfec3fa3c2dd1776226c4
Miguel Gasca <miguelgasca@gmail.com>
2017-03-04 23:35:53 +0100

    Interval & timeout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,12 @@
 var balance = 10000;
-var topBalance;
+var targetBalance;
 var numsToUpdate;
 var meter = Meter({selector: 'meter'});
 var start;
 meter.init({balance: balance});
 
 $('#newBalance').on('click', requestNewBalance);
+$('#restoreBalance').on('click', restoreBalance);
 
 var interval;
 var timeout = setTimeout(requestNewBalance, 30000);
@@ -15,11 +16,10 @@ var lastTry = 0;
 function refreshBalance(inc) {
     console.log(inc);
     balance += inc;
-    console.log(balance);
-    if (balance <= topBalance) {
+    if (balance <= targetBalance) {
         meter.setBalance(balance);
     } else {
-        meter.setBalance(topBalance);
+        meter.setBalance(targetBalance);
         clearInterval(interval);
         console.log('end ' + (new Date() - start)/1000);
         timeout = setTimeout(requestNewBalance, 30000);
@@ -30,18 +30,23 @@ function requestNewBalance() {
     clearInterval(interval);
     clearTimeout(timeout);
     start = new Date();
-    topBalance = serverRequest();
-    numsToUpdate = ((topBalance - balance));
-    var milliSecondsToUpdate = 30000;
-    var increment = (numsToUpdate / milliSecondsToUpdate) * 10;
+    targetBalance = serverRequest();
+    numsToUpdate = ((targetBalance - balance));
+    var tick = 10;
+    var milliSecondsToUpdate = 30000 / tick;
+    var increment = (numsToUpdate / milliSecondsToUpdate);
     interval = setInterval(function () {
         refreshBalance(increment);
-    }, 10);
+    }, tick);
 }
 
 function serverRequest() {
-    // var newBalance = 1010000 + (100 * lastTry);
-    var newBalance = 144000;
+    var newBalance = balance + (balance * 0.001);
+    // var newBalance = 1000000;
     lastTry++;
     return newBalance;
+}
+
+function restoreBalance() {
+    balance = 10000;
 }
